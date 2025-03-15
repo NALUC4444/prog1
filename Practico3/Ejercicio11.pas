0f16a706e155e3fec57e377773068fd1531fb156
LevitateGamesStudio <segrobn@gmail.com>
2025-03-15 17:30:21 -0300

    Cambios realizados

---
+++ Practico3/Ejercicio11.pas
@@ -0,0 +1,10 @@
+(*Letra Ejercicio
+Escriba un programa en Pascal que lea de la entrada estandar un entero positivo n de
+exactamente cuatro dıgitos y otro entero positivo d de exactamente 1 dıgito. Si d aparece
+en la representacion decimal de n, entonces exhiba en la salida estandar el numero n con
+un signo de suma (+) debajo de cada aparicion de d. En caso contrario, exhiba el mensaje
+’d no aparece en n’. Su programa no necesita controlar que los enteros ingresados tengan
+cada uno la cantidad de cifras indicada, asuma que ası sera. Incluya mensajes de salida con
+etiquetas descriptivas para el ingreso y el despliegue de datos.*)
+program P3Ej11;
+CONST SeRepite = '+';
