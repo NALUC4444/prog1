8925d117f765f426c4a88bac92cf7cafab0962e4
NALUC4444 <163031421+NALUC4444@users.noreply.github.com>
2025-03-15 17:23:33 -0300

    Delete Python directory

--- Python/Juego.py
+++
@@ -1,6 +0,0 @@
-from pyrae import dle
-
-PalabraBuscar = input("Qué palabra deseas buscar?\n")
-resultadoBusqueda = dle.search_by_word(word=PalabraBuscar)
-print(f"La palabra era: {PalabraBuscar}")
-print(resultadoBusqueda)
