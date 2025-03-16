7577219b54f8cf5bd77804cab4e5c5208e258373
LevitateGamesStudio <segrobn@gmail.com>
2025-03-15 23:06:21 -0300

    New Update

---
+++ Practico3/Ejercicio10.pas
@@ -0,0 +1,23 @@
+(*Letra Ejercicio:
+El costo de enviar por correo un paquete es
+- $77 para paquetes que pesan hasta un kilogramo (inclusive)
+- $77 mas $56 por kilogramo adicional o fraccion para paquetes que pesan mas de un
+kilogramo.
+Escriba un programa en Pascal que, lea de la entrada estandar el peso de un paquete como
+un numero real de kilogramos y exhiba en la salida estandar dicho peso junto al costo del
+envio, segun se muestra en los siguientes ejemplos. Incluya mensajes de salida con etiquetas
+descriptivas para el ingreso y el despliegue de datos.*)
+
+program P3Ej10;
+CONST 
+    envio = 77; //Monto fijo
+    Extra = 56; //Tarifa extra
+var
+    peso, precio : real;
+begin
+    write('Ingrese peso del paquete: ');
+    readln(peso);
+    if (peso <= 1.00) then precio := envio; //Dado por la letra
+    writeln('   Peso ', ' Costo');
+    writeln(peso:7:2,precio:7:2);
+end.
