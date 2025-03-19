b0cf6f4f37a3795f49a92a8abe0d1ed660329716
LevitateGamesStudio <segrobn@gmail.com>
2025-03-19 00:12:43 -0300

    EjercicioCompleto

--- Practico2/Ejercicio6.pas
+++ Practico2/Ejercicio6.pas
@@ -9,37 +9,26 @@ CONST
     MaxHoras = 24;
     MaxMinutos = 60;
 var
-    num1, num2, m1, m2, h1, h2, d, resultadoH, resultadoM : integer;
+    num1, num2, resultado, restoH, restoM : integer;
 
 begin
     write('Ingrese el primer valor: ');
     readln(num1);
     write('Ingrese el segundo valor: ');
     readln(num2);
-    m1 := num1 MOD 100; {Primeros dos minutos}
-    h1 := num1 DIV 100; {Primeros dos horas}
-    m2 := num2 MOD 100; {Segundos dos minutos}
-    h2 := num2 DIV 100; {Segundos dos horas}
-    resultadoH := h1 + h2;
-    resultadoM := m1 + m2;
-    if (resultadoH > MaxHoras) then
+    resultado := num1 + num2;
+    restoM := resultado MOD 100; //Primeros dos minutos
+    restoH := resultado DIV 100; //Primeros dos horas
+    if (restoH > MaxHoras) then
     begin
-
-        writeln('El resultado es: 1 ', resultadoH, resultadoM);
+        restoM := restoM - 60; //Obtener valores restantes
+        restoH := restoH - 23; //Obtener valores restantes
+        writeln('El resultado es: 1 ', restoH, restoM);
     end
     else
-        writeln('El resultado es: 0 ', resultadoH, resultadoM);
+        writeln('El resultado es: 0 ', restoH, restoM);
 end.
 
 
 {Entrada     Salida
 1345 2153    1 1138
-
-
-1345
-2153+
-------
-3498}
-
-//No agarrar por separado y tratar el numero entero completo si se pasa de los limites luego
-//se analiza
