d02c23d7ecedb74c6d3fa8d02251adf0795d57e8
LevitateGamesStudio <segrobn@gmail.com>
2025-03-21 23:23:15 -0300

    Comienza Ejercicio

---
+++ Practico5/Ejercicio5.pas
@@ -0,0 +1,17 @@
+(*Letra Ejercicio:
+Se desea implementar un programa que calcule el saldo final de una cuenta. Suponga que
+los datos son leidos de la entrada estandar y que constan de renglones. El primer renglon
+contiene el saldo inicial de la cuenta. Los siguientes renglones contienen una letra y un valor
+real para indicar las transacciones (posiblemente ninguna). La letra puede ser la D para
+efectuar un deposito o la R para efectuar un retiro. El ultimo renglon contiene unicamente la
+letra X. Escriba un programa en Pascal que determine el saldo exacto de la cuenta despues
+de procesar las transacciones. Incluya mensajes de salida con etiquetas descriptivas para
+exhibir los valores.*)
+program P5Ej5;
+CONST centinela = 'X';
+var
+    Eleccion : char;
+    SaldoActual, Operacion : integer;
+begin
+    
+end.
