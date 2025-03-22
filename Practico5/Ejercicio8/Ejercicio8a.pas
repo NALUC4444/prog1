0acb9f380fecacfd09c921430046c5a55ee39f71
LevitateGamesStudio <segrobn@gmail.com>
2025-03-22 18:59:10 -0300

    EjercicioEmpezado

--- Practico5/Ejercicio8/Ejercicio8a.pas
+++ Practico5/Ejercicio8/Ejercicio8a.pas
@@ -2,12 +2,21 @@
 (a) Escriba un programa en Pascal que resuelva lo pedido, suponiendo que no hay espacios
 en la entrada.*)
 procedure CalcularConEspacios();
-var operador, valor : char;
+var 
+    operador, valorAnterior, valorActual : char;
+    resultado : integer;
 begin
     writeln('Ingrese la cuenta a realizar: ');
-    read(valor);
-    while valor <> centinela do
+    read(valorActual);
+    read(operador);
+    while operador <> centinela do
     begin
-        
+        valorAnterior := valorActual;
+        case operador of
+            '+' : resultado := (ord(valorAnterior) - ord('0')) + resultado;
+            '-' : resultado := (ord(valorAnterior) - ord('0')) - resultado;
+            '/' : resultado := (ord(valorAnterior) - ord('0')) / resultado;
+            '*' : resultado := (ord(valorAnterior) - ord('0')) * resultado;
+        end;
     end;
 end;
