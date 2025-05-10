c3de234c73943c9ea52743ef89baf145bfc62ea8
LevitateGamesStudio <segrobn@gmail.com>
2025-05-10 18:19:03 -0300

    Ejercicio4 Completo

---
+++ Practico9/Ejercicio4/Ejercicio4.pas
@@ -0,0 +1,36 @@
+program P9Ej4;
+type
+    TipoMatriz = array[1..10, 1..10] of integer; {Primero son columna y segundo son fila}
+    TipoAuxiliar = array[1..10] of integer; {Se usa en el segundo procedimiento}
+
+var 
+    m, n : integer;
+    matriz : TipoMatriz;
+
+{$INCLUDE Ejercicio4b.pas}
+
+{$INCLUDE Ejercicio4c.pas}
+
+procedure mostrar(matriz : TipoMatriz);
+var i, j : integer;
+begin
+    writeln();
+    for i := 1 to 10 do
+    begin
+        for j := 1 to 10 do
+        begin
+            write(matriz[i, j], ' ');
+        end;
+        readln();
+    end;
+end;
+
+begin
+    cargarMatriz(matriz);
+    write('Ingrese fila m: ');
+    readln(m);
+    write('Ingrese fila n: ');
+    readln(n);
+    intercambio(m, n, matriz);
+    mostrar(matriz);
+end.
---
+++ Practico9/Ejercicio4/Ejercicio4c.pas
@@ -0,0 +1,25 @@
+{Letra:
+Escriba un procedimiento que dada una matriz de tipo TipoMatriz y 
+dos variables enteras m y n,
+intercambie las filas m y n de la matriz. Es decir, todos los 
+valores de la fila m deben quedar en la fila n y viceversa.}
+procedure intercambio(m, n : integer; VAR matriz : TipoMatriz);
+var 
+    auxMatriz : TipoAuxiliar;
+    i : integer;
+begin
+    for i := 1 to 10 do
+    begin
+        auxMatriz[i] := matriz[i, m]; {Auxiliar de resplado}
+        matriz[i, m] := matriz[i, n];
+        matriz[i, n] := auxMatriz[i]; {Ya que el valor lo moví a la anterior línea}
+    end;
+end;
+
+{
+1 2 3 4 5 6 7 8
+9 8 7 6 5 4 3 2 = m => m = 2
+1 2 3 4 5 6 7 8 = n => n = 3
+
+9 8 7 6 5 4 3 2 = Aux => Aux = m = 2
+}
